8199b8b90ae5a2d5964e5a1c7e7f24ee9cb36c29
Rushabh Vasani <vasanirushabh24@gmail.com>
2021-04-06 18:43:14 +0530

    add statements for debugging

--- layoutlm/layoutlm/data/funsd.py
+++ layoutlm/layoutlm/data/funsd.py
@@ -231,7 +231,7 @@ def convert_examples_to_features(
     """
 
     label_map = {label: i for i, label in enumerate(label_list)}
-
+    print(label_map, max_seq_length)
     features = []
     for (ex_index, example) in enumerate(examples):
         file_name = example.file_name
